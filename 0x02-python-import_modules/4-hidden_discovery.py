5ddcc61b39ff73833f7d92b1677287a447acae6e
razaan96 <razaan.alaa@hotmail.com>
2023-11-30 03:59:25 -0800

    Who are you

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from hidden_4 import *
+    arr = dir()
+    for i in range(0, len(arr)):
+        if arr[i][:2] != "__":
+            print("{:s}".format(arr[i]))
