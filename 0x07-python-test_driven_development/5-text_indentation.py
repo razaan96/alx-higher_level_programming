640225c295cd5b0f0388a3bc17ff156f1a9282fd
razaan96 <razaan.alaa@hotmail.com>
2024-01-08 12:07:32 -0800

    4

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,37 +1,27 @@
 #!/usr/bin/python3
-
-"""
-Text Indentation module
-This module adds two new lines after these characters: ".?:"
-and doesn't print any spaces at the beginning or end
-of the given sentence.
-Prototype: def text_indentation(text):
-Raise exceptions if text is not a string.
-"""
+"""Module for text_indentation method."""
 
 
 def text_indentation(text):
-    """
-    Function that prints 2 new lines after these chars:
-    ".", "?" and ":"
+    """Method for adding 2 new lines after '.?:' chars.
+
+    Args:
+        text: The str text.
+
+    Raises:
+        TypeError: If text is not a str.
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    # initialize variable to hold final string
-    final_str = ""
+    for delim in ".?:":
+        # print(delim, text.split(delim))
+        text = (delim + "\n\n").join(
+            [line.strip(" ") for line in text.split(delim)])
 
-    # flag to skip/trim leading whitespaces
-    trim_spaces = True
+    print(text, end="")
 
-    # iterate through all chars in the given text
-    for char in text:
-        # check if char is one of these: '.', '?' or ':'
-        if char in ".?:":
-            final_str += char + "\n\n"  # add char and two new lines
-            trim_spaces = True  # set flag to skip leading whitespaces
-        elif char != " " or not trim_spaces:
-            final_str += char
-            trim_spaces = False  # reset flag if it's a non-space character
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/5-text_indentation.txt")
 
-    print(final_str)
